204cb8d6a101a2776e0ab3ca7c158fae4b126e3e
ALEKS G <aleksandrgaranin89@gmail.com>
2020-12-18 10:26:46 -0600

    request/response basic

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.write('hello there!');
-    response.end()
+    let body = [];
+    // console.log(request.method, request.url)
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    });
+    request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        if(body){
+            userName = body.split('=')[1];
+        } else {
+            userName = "User"
+        }
+        console.log(body, userName);
+        response.setHeader('Content-Type', 'text/html');
+        response.write(`<h1>hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Submit</button></form>`);
+        response.end()
+
+    })
+
 });
 
 server.listen(3000);
